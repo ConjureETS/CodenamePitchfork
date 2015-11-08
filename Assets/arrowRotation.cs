cd0e991fafac51828d67bd98d12f501d69bd68b3
marcmad <marcmad12345@gmail.com>
2015-11-08 15:03:45 -0500

    Merged the 2 scenes. Added the directionnal arrow correctly.

--- Assets/arrowRotation.cs
+++ Assets/arrowRotation.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class arrowRotation : MonoBehaviour {
     GameObject target;
+    GameObject player;
 	// Use this for initialization
 	void Start () {
         target = GameObject.Find("Objective");
+        player = GameObject.Find("FPSController");
 	}
 	
 	// Update is called once per frame
@@ -16,14 +18,14 @@ public class arrowRotation : MonoBehaviour {
         // distance between target and the actual rotating object
         Vector3 D = target.transform.position - transform.position;
 
-
+        Debug.Log(player.transform.rotation);
         // calculate the Quaternion for the rotation
-        Quaternion rot = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(D), rotSpeed * Time.deltaTime);
+        Quaternion rot = Quaternion.Slerp(player.transform.rotation, Quaternion.LookRotation(D), rotSpeed * Time.deltaTime);
 
         //Apply the rotation 
         transform.rotation = rot;
 
         // put 0 on the axys you do not want for the rotation object to rotate
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z); 
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z); 
 	}
 }
